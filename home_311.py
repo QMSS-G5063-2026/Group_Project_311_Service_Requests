b38bb84476e592a0dbc4ab43eff12bebfe598ae3
Maryla Wozniak-Odomirok <48894916+molinexx@users.noreply.github.com>
2026-04-19 11:39:10 -0400

    Update home_311.py add header

--- home_311.py
+++ home_311.py
@@ -1,16 +1,33 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 17 16:17:41 2026
-
-@author: mw3595
-"""
-
 import streamlit as st
-import numpy as np
 import pandas as pd
 
-st.title("My First App")
+st.markdown("""
+<div style="
+    background: linear-gradient(90deg, #1f77b4, #4aa3df);
+    padding: 25px;
+    border-radius: 10px;
+    color: white;
+">
+    <h1 style="margin-bottom: 10px;">
+        NYC 311 Neighborhood Issues Explorer
+    </h1>
+    <p style="font-size:18px;">
+        Explore New York City using 311 complaint data by borough, neighborhood, audience group, or complaint type.
+    </p>
+    <p style="font-size:16px;">
+        Designed to help residents, researchers, and decision-makers understand quality-of-life issues through maps, trends, comparisons, and future analytics.
+    </p>
+    <p style="font-size:14px; opacity:0.85;">
+        Created by  Kenneth Shelton, Maryla Wozniak, and Yawar Sheikh
+    </p>
+</div>
+""", unsafe_allow_html=True)
 
-df = pd.read_csv("NYC_311_Master_2025_mw2.csv")
+df = pd.read_csv("NYC_311_Master_2024_2025.csv")
 
 st.write(df)
+
+col1, col2 = st.columns(2)
+
+col1.metric("Rows", df.shape[0])
+col2.metric("Columns", df.shape[1])
